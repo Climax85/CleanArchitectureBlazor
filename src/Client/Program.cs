09fea7e3148745fe345e55274ff5b5c0072992cf
Patrick W <climax85@web.de>
2022-02-03 21:24:12 +0100

    CSharp Client generieren und in Client einbinden

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorUI.Services;
 using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient(factory =>
+    new TodoListsClient("https://localhost:5001", factory.GetRequiredService<HttpClient>()));
+builder.Services.AddTransient(factory =>
+    new TodoItemsClient("https://localhost:5001", factory.GetRequiredService<HttpClient>()));
 
 await builder.Build().RunAsync();
