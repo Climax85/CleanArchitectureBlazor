813b74b9cc0eef7228c7e31d57af1d7a4c439d22
Patrick W <climax85@web.de>
2022-02-03 21:02:14 +0100

    Add SerializedNotification with JsonConverter for SignalR connection

---
+++ src/Application.Shared/Common/EventHandlers/NotificationJsonConverter.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Application.Shared.Common.EventHandlers;
+
+public class NotificationJsonConverter : JsonConverter<SerializedNotification>
+{
+    private readonly IEnumerable<Type> _types;
+
+    public NotificationJsonConverter()
+    {
+        var type = typeof(SerializedNotification);
+        _types = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => type.IsAssignableFrom(p) && p.IsClass && !p.IsAbstract)
+            .ToList();
+        Console.WriteLine(string.Join("\n", _types));
+    }
+
+    public override SerializedNotification Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        Console.WriteLine("SerializedNotification.Read");
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+        Console.WriteLine("SerializedNotification.Read2");
+
+        using (var jsonDocument = JsonDocument.ParseValue(ref reader))
+        {   
+            if (!jsonDocument.RootElement.TryGetProperty("notificationType", out var typeProperty))
+            {
+                Console.WriteLine("SerializedNotification.notificationType not found");
+                throw new JsonException();
+            }
+            var type = _types.FirstOrDefault(x => x.Name == typeProperty.GetString());
+            if (type == null)
+            {
+                throw new JsonException();
+            }
+
+            Console.WriteLine($"SerializedNotification.type: {type}");
+
+            var jsonObject = jsonDocument.RootElement.GetRawText();
+            Console.WriteLine($"SerializedNotification.jsonObject: {jsonObject}");
+            try
+            {
+                var result = JsonSerializer.Deserialize(jsonObject, type, options);
+                Console.WriteLine($"SerializedNotification.result: {result.GetType().FullName}");
+                return (SerializedNotification)result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, SerializedNotification value, JsonSerializerOptions options)
+    {
+        Console.WriteLine("SerializedNotification.Write");
+        JsonSerializer.Serialize(writer, (object)value, options);
+    }
+}
---
+++ src/Application.Shared/Common/EventHandlers/SerializedNotification.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Application.Shared.Common.EventHandlers;
+
+public abstract class SerializedNotification : INotification
+{
+    public string NotificationType
+    {
+        get
+        {
+            return this.GetType().Name;
+        }
+        set{}
+    }
+}
