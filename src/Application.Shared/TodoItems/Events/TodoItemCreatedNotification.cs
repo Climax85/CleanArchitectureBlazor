c34cbe0302157083c4ec32ae96c13abfa34b3cbc
Patrick W <climax85@web.de>
2022-02-03 21:08:58 +0100

    Send TodoItemCreatedNotification from DomainEventHandler

---
+++ src/Application.Shared/TodoItems/Events/TodoItemCreatedNotification.cs
@@ -0,0 +1,14 @@
+﻿using Application.Shared.Common.EventHandlers;
+using Application.Shared.TodoLists.Queries.GetTodos;
+
+namespace Application.Shared.TodoItems.Events;
+
+public class TodoItemCreatedNotification : SerializedNotification
+{
+    public TodoItemCreatedNotification(TodoItemDto item)
+    {
+        Item = item;
+    }
+
+    public TodoItemDto Item { get; }
+}
--- src/Application/TodoItems/EventHandlers/TodoItemCreatedEventHandler.cs
+++ src/Application/TodoItems/EventHandlers/TodoItemCreatedEventHandler.cs
@@ -1,4 +1,8 @@
-﻿using CleanArchitecture.Application.Common.Models;
+﻿using Application.Shared.TodoItems.Events;
+using Application.Shared.TodoLists.Queries.GetTodos;
+using AutoMapper;
+using CleanArchitecture.Application.Common.Hubs;
+using CleanArchitecture.Application.Common.Models;
 using CleanArchitecture.Domain.Events;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -8,18 +12,25 @@ namespace CleanArchitecture.Application.TodoItems.EventHandlers;
 public class TodoItemCreatedEventHandler : INotificationHandler<DomainEventNotification<TodoItemCreatedEvent>>
 {
     private readonly ILogger<TodoItemCreatedEventHandler> _logger;
+    private readonly IMapper _mapper;
+    private readonly NotificationHub _notificationHub;
 
-    public TodoItemCreatedEventHandler(ILogger<TodoItemCreatedEventHandler> logger)
+    public TodoItemCreatedEventHandler(ILogger<TodoItemCreatedEventHandler> logger, NotificationHub notificationHub,
+        IMapper mapper)
     {
         _logger = logger;
+        _notificationHub = notificationHub;
+        _mapper = mapper;
     }
 
-    public Task Handle(DomainEventNotification<TodoItemCreatedEvent> notification, CancellationToken cancellationToken)
+    public async Task Handle(DomainEventNotification<TodoItemCreatedEvent> notification,
+        CancellationToken cancellationToken)
     {
         var domainEvent = notification.DomainEvent;
 
         _logger.LogInformation("CleanArchitecture Domain Event: {DomainEvent}", domainEvent.GetType().Name);
 
-        return Task.CompletedTask;
+        await _notificationHub.SendNotification(
+            new TodoItemCreatedNotification(_mapper.Map<TodoItemDto>(domainEvent.Item)));
     }
 }
