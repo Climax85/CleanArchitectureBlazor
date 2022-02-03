fe9a93af54dba0107d3132b1e2749ff33b8a695d
Patrick W <climax85@web.de>
2022-02-03 21:26:05 +0100

    AutoMapper in Client hinzufügen

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using BlazorUI.Services;
 using Client;
 using Microsoft.AspNetCore.Components.Web;
@@ -6,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTransient(factory =>
